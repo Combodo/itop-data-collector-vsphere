6d18f7ea45893c5d8baadb5d204b1cd64c985f4c
Denis Flaven <denis.flaven@combodo.com>
2015-02-13 17:36:19 +0000

    version 1.0.1: Must load servers BEFORE hypervisors
    SVN:trunk[4362]

--- module.itop-collector-vsphere.php
+++ module.itop-collector-vsphere.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-collector-vsphere/1.0.0',
+	'itop-collector-vsphere/1.0.1',
 	array(
 		// Identification
 		//
