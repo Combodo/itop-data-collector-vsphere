feef9ef6c52ee6534e0c34342fa737fbab6ca448
xtophe38 <christophe.naud@gmail.com>
2022-09-23 11:27:12 +0200

    Increase revision number

--- module.itop-collector-vsphere.php
+++ module.itop-collector-vsphere.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-collector-vsphere/1.0.13',
+	'itop-collector-vsphere/1.0.14',
 	array(
 		// Identification
 		//
