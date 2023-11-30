b94dbff6faafe26a537a712f703a6fe8b8b84d32
xtophe38 <christophe.naud@gmail.com>
2023-11-30 12:06:14 +0100

    Skip network collecting info if these infos are not set

--- module.itop-data-collector-vsphere.php
+++ module.itop-data-collector-vsphere.php
@@ -16,7 +16,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-data-collector-vsphere/1.1.0',
+	'itop-data-collector-vsphere/1.1.1-dev',
 	array(
 		// Identification
 		//
