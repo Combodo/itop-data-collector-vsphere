0f53bb1164da38c481a712c08ae06b396ff80297
xtophe38 <13520055+xtophe38@users.noreply.github.com>
2025-01-17 09:52:21 +0100

    Update vSphereIPv6AddressCollector.class.inc.php
    Use filter_var for IPs consistently across the files.

--- src/vSphereIPv6AddressCollector.class.inc.php
+++ src/vSphereIPv6AddressCollector.class.inc.php
@@ -67,8 +67,8 @@ class vSphereIPv6AddressCollector extends vSphereCollector
 		if (class_exists('vSphereVirtualMachineCollector')) {
 			$aVMs = vSphereVirtualMachineCollector::CollectVMInfos();
 			foreach ($aVMs as $oVM) {
-				$sIP = $oVM['managementip_id'] ?? '';
-				if (filter_var($sIP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+				$sIP = filter_var($oVM['managementip'] ?? '', FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ?: '';
+				if ($sIP != '') {
 					Utils::Log(LOG_DEBUG, 'IPv6 Address: ' . $sIP);
 					if (in_array('short_name', $oVM)) {
 						$sShortName = explode('.', $oVM['short_name'])[0];  // Remove chars after '.', if any
@@ -90,8 +90,8 @@ class vSphereIPv6AddressCollector extends vSphereCollector
 		if (class_exists('vSphereServerCollector')) {
 			$aServers = vSphereServerCollector::CollectServerInfos();
 			foreach ($aServers as $oServer) {
-				$sIP = $oServer['managementip_id'] ?? '';
-				if (filter_var($sIP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+				$sIP = filter_var($oServer['managementip_id'] ?? '', FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ?: '';
+				if ($sIP != '') {
 					Utils::Log(LOG_DEBUG, 'IPv6 Address: ' . $sIP);
 					$this->aIPv6Addresses[] = array(
 						'id' => $sIP,
@@ -108,8 +108,8 @@ class vSphereIPv6AddressCollector extends vSphereCollector
 		if ($this->oCollectionPlan->GetTeemIpOption('manage_logical_interfaces') && class_exists('vSpherelnkIPInterfaceToIPAddressCollector')) {
 			$aLnkInterfaceIPAddressses = vSpherelnkIPInterfaceToIPAddressCollector::GetLnks();
 			foreach ($aLnkInterfaceIPAddressses as $oLnkInterfaceIPAddresss) {
-				$sIP = $oLnkInterfaceIPAddresss['ipaddress_id'] ?? '';
-				if (filter_var($sIP, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6)) {
+				$sIP = filter_var($oLnkInterfaceIPAddresss['ipaddress_id'] ?? '', FILTER_VALIDATE_IP, FILTER_FLAG_IPV6) ?: '';
+				if ($sIP != '') {
 					// Check if address is already listed as it may be that vSphere reported it as management IP too
 					// Don't register duplicates otherwise
 					$sKey = false;
