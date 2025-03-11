c5cdf5aab0b5385c181a0663dc31c963f154d9f0
Anne-Cath <anne-catherine.cognet@combodo.com>
2025-03-11 13:52:26 +0100

    Updated May 15, 2015 version of the library (latest on the official repository)

--- library/Vmwarephp/Vhost.php
+++ library/Vmwarephp/Vhost.php
@@ -5,7 +5,6 @@ use Vmwarephp\Exception as Ex;
 
 class Vhost {
 	private $service;
-    private array $aProperties = [];
 
 	function __construct($host, $username, $password) {
 		$this->host = $host;
@@ -18,15 +17,15 @@ class Vhost {
 		return $port ? : '443';
 	}
 
-    function __get($propertyName) {
-        if (!array_key_exists($propertyName, $this->aProperties)) throw new \InvalidArgumentException('Property ' . $propertyName . ' not set on this object!');
-        return $this->aProperties[$propertyName];
-    }
+	function __get($propertyName) {
+		if (!isset($this->$propertyName)) throw new \InvalidArgumentException('Property ' . $propertyName . ' not set on this object!');
+		return $this->$propertyName;
+	}
 
-    function __set($propertyName, $value) {
-        $this->validateProperty($propertyName, $value);
-        $this->aProperties[$propertyName] = $value;
-    }
+	function __set($propertyName, $value) {
+		$this->validateProperty($propertyName, $value);
+		$this->$propertyName = $value;
+	}
 
 	function __call($method, $arguments) {
 		if (!$this->service) $this->initializeService();
@@ -37,10 +36,6 @@ class Vhost {
 		return $this->getServiceContent()->about->apiType;
 	}
 
-	function getApiVersion() {
-		return $this->getServiceContent()->about->apiVersion;
-	}
-	
 	function changeService(\Vmwarephp\Service $service) {
 		$this->service = $service;
 	}
