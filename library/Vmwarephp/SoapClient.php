9cf6dc68a8953609da606e1fa5fe427887e11f41
Anne-Cath <anne-catherine.cognet@combodo.com>
2025-03-12 08:43:44 +0100

    Add proxy param

--- library/Vmwarephp/SoapClient.php
+++ library/Vmwarephp/SoapClient.php
@@ -1,21 +1,34 @@
 <?php
 namespace Vmwarephp;
-
 class SoapClient extends \SoapClient {
+    function __doRequest($request, $location, $action, $version, $one_way = 0) {
+        $request = $this->appendXsiTypeForExtendedDatastructures($request);
+        $headers = parent::__getLastResponseHeaders();
+        $arHeaders = explode(PHP_EOL, $headers);
+        foreach ($arHeaders as $value)
+        {
+            $val = explode(':', $value);
+            $arHeaders[ strtolower($val[0]) ] = (isset($val[1])) ? trim($val[1]) : '';
+        }
 
-	function __doRequest($request, $location, $action, $version, $one_way = 0) {
-		$request = $this->appendXsiTypeForExtendedDatastructures($request);
-		$result = parent::__doRequest($request, $location, $action, $version, $one_way);
-		if (isset($this->__soap_fault) && $this->__soap_fault) {
-			throw $this->__soap_fault;
-		}
-		return $result;
-	}
+        if (!empty($arHeaders['set-cookie']))
+        {
+            $re = '/(.+)="(.+)";/m';
+            preg_match_all($re, $arHeaders['set-cookie'], $matches, PREG_SET_ORDER, 0);
+            parent::__setCookie('vmware_soap_session', '"'.$matches[0][2].'"');
+        }
 
-	/* PHP does not provide inheritance information for wsdl types so we have to specify that its and xsi:type
-	 * php bug #45404
-	 * */
-	private function appendXsiTypeForExtendedDatastructures($request) {
-		return $request = str_replace(array("xsi:type=\"ns1:TraversalSpec\"", '<ns1:selectSet />'), array("xsi:type=\"ns1:TraversalSpec\"", ''), $request);
-	}
+        $result = parent::__doRequest($request, $location, $action, $version, $one_way);
+
+        if (isset($this->__soap_fault) && $this->__soap_fault) {
+            throw $this->__soap_fault;
+        }
+        return $result;
+    }
+    /* PHP does not provide inheritance information for wsdl types so we have to specify that its and xsi:type
+     * php bug #45404
+     * */
+    private function appendXsiTypeForExtendedDatastructures($request) {
+        return $request = str_replace(array("xsi:type=\"ns1:TraversalSpec\"", '<ns1:selectSet />'), array("xsi:type=\"ns1:TraversalSpec\"", ''), $request);
+    }
 }
