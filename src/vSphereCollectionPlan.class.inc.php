46fac9d5bbefeb138a4028fa4ee0a3f3958146a9
Thomas Casteleyn <thomas.casteleyn@super-visions.com>
2025-09-04 12:28:30 +0200

    Improve module installation checks by using integrated methods

--- src/vSphereCollectionPlan.class.inc.php
+++ src/vSphereCollectionPlan.class.inc.php
@@ -25,88 +25,20 @@ class vSphereCollectionPlan extends CollectionPlan
 
 		$oRestClient = new RestClient();
 
-		// Get last installation date
-		$sLastInstallDate = '0000-00-00 00:00:00';
-		try {
-			$aDatamodelResults = $oRestClient->Get('ModuleInstallation', ['name' => 'datamodel'], 'installed', 1);
-			if ($aDatamodelResults['code'] != 0 || empty($aDatamodelResults['objects'])) {
-				throw new Exception($aDatamodelResults['message'], $aDatamodelResults['code']);
-			}
-			$aDatamodel = current($aDatamodelResults['objects']);
-			$sLastInstallDate = $aDatamodel['fields']['installed'];
-		} catch (Exception $e) {
-			$sMessage = sprintf('Last Datamodel installation date is considered as not defined due to %s', $e->getMessage());
-			Utils::Log(LOG_ERR, $sMessage);
-		}
-
 		// Check if Virtualization Management Module is installed
 		Utils::Log(LOG_INFO, '---------- Check Virtualization Management Module installation ----------');
-		$this->bVirtualizationMgtIsInstalled = false;
-		try {
-            $aResult = $oRestClient->Get('ModuleInstallation', "SELECT ModuleInstallation WHERE name = 'iTop-virtualization-mgmt' AND installed >= '$sLastInstallDate'", 'version, installed', 1);
-			if (array_key_exists('objects', $aResult) && isset($aResult['objects'])) {
-				$this->bVirtualizationMgtIsInstalled = true;
-				$aObject = current($aResult['objects']);
-				$this->sVirtualizationMgtVersion = $aObject['fields']['version'];
-				$sVirtualizationMgmtMessage = 'Virtualization Management Module version '.$this->sVirtualizationMgtVersion.' is installed';
-			} else {
-				$this->sVirtualizationMgtVersion = 'unknown';
-				$sVirtualizationMgmtMessage = 'Virtualization Management Module is not installed';
-			}
-		} catch (Exception $e) {
-			$sMessage = 'Virtualization Management Module\'s installed version cannot be fetched: '.$e->getMessage();
-			if (is_a($e, "IOException")) {
-				Utils::Log(LOG_ERR, $sMessage);
-				throw $e;
-			}
-		}
-		Utils::Log(LOG_INFO, $sVirtualizationMgmtMessage);
+		$this->bVirtualizationMgtIsInstalled = Utils::CheckModuleInstallation('itop-virtualization-mgmt', false, $oRestClient);
+		$this->sVirtualizationMgtVersion = $this->bVirtualizationMgtIsInstalled ? Utils::GetModuleVersion('itop-virtualization-mgmt') : 'unknown';
 
 		// Check if Data model for vSphere  is installed
 		Utils::Log(LOG_INFO, '---------- Check Data model for vSphere installation ----------');
-		$this->bCbdVMwareDMIsInstalled = false;
-		try {
-            $aResult = $oRestClient->Get('ModuleInstallation', "SELECT ModuleInstallation WHERE name = 'combodo-vsphere-datamodel' AND installed >= '$sLastInstallDate'", 'version, installed', 1);
-			if (array_key_exists('objects', $aResult) && isset($aResult['objects'])) {
-				$this->bCbdVMwareDMIsInstalled = true;
-				$aObject = current($aResult['objects']);
-				$this->sCbdVMwareDMVersion = $aObject['fields']['version'];
-				$sCbdVMwareDMMessage = 'Data model for vSphere extension version '.$this->sCbdVMwareDMVersion.' is installed';
-			} else {
-				$this->sCbdVMwareDMVersion = 'unknown';
-				$sCbdVMwareDMMessage = 'Data model for vSphere extension is not installed';
-			}
-		} catch (Exception $e) {
-			$sMessage = 'Data model for vSphere extension\'s installed version cannot be fetched: '.$e->getMessage();
-			if (is_a($e, "IOException")) {
-				Utils::Log(LOG_ERR, $sMessage);
-				throw $e;
-			}
-		}
-		Utils::Log(LOG_INFO, $sCbdVMwareDMMessage);
+		$this->bCbdVMwareDMIsInstalled = Utils::CheckModuleInstallation('combodo-vsphere-datamodel', false, $oRestClient);
+		$this->sCbdVMwareDMVersion = $this->bCbdVMwareDMIsInstalled ? Utils::GetModuleVersion('combodo-vsphere-datamodel') : 'unknown';
 
-		// Check if TeemIp is installed or not
-		Utils::Log(LOG_INFO, '---------- Check TeemIp installation ----------');
-		$this->bTeemIpIsInstalled = false;
-		try {
-            $aResult = $oRestClient->Get('ModuleInstallation', "SELECT ModuleInstallation WHERE name = 'teemip-ip-mgmt' AND installed >= '$sLastInstallDate'", 'version, installed', 1);
-			if (array_key_exists('objects', $aResult) && isset($aResult['objects'])) {
-				$this->bTeemIpIsInstalled = true;
-				$aObject = current($aResult['objects']);
-				$this->sTeemIpVersion = $aObject['fields']['version'];
-				$sTeemIpMessage = 'TeemIp version ' . $this->sTeemIpVersion . ' is installed';
-			} else {
-				$this->sTeemIpVersion = 'unknown';
-				$sTeemIpMessage = 'TeemIp is NOT installed';
-			}
-		} catch (Exception $e) {
-			$sMessage = 'TeemIp is considered as NOT installed due to: ' . $e->getMessage();
-			if (is_a($e, "IOException")) {
-				Utils::Log(LOG_ERR, $sMessage);
-				throw $e;
-			}
-		}
-		Utils::Log(LOG_INFO, $sTeemIpMessage);
+		// Check if TeemIP is installed or not
+		Utils::Log(LOG_INFO, '---------- Check TeemIP installation ----------');
+		$this->bTeemIpIsInstalled = Utils::CheckModuleInstallation('teemip-ip-mgmt', false, $oRestClient);
+		$this->sTeemIpVersion = $this->bTeemIpIsInstalled ? Utils::GetModuleVersion('teemip-ip-mgmt') : 'unknown';
 
 		// If TeemIp should be considered,
 		$this->bTeemIpIpDiscoveryIsInstalled = false;
@@ -114,68 +46,19 @@ class vSphereCollectionPlan extends CollectionPlan
 		$this->bTeemIpZoneMgmtIsInstalled = false;
 		$aTeemIpDiscovery = Utils::GetConfigurationValue('teemip_discovery', []);
 		if ($this->bTeemIpIsInstalled && !empty($aTeemIpDiscovery) && array_key_exists('enable', $aTeemIpDiscovery) && ($aTeemIpDiscovery['enable'] == 'yes')) {
-			Utils::Log(LOG_INFO, 'TeemIp should be considered.');
+			Utils::Log(LOG_INFO, 'TeemIP should be considered.');
 			// Record discovery parameters
 			$this->bCollectIps = array_key_exists('collect_ips', $aTeemIpDiscovery) ? $aTeemIpDiscovery['collect_ips'] : 'no';
 			$this->sDefaultIpStatus = array_key_exists('default_ip_status', $aTeemIpDiscovery) ? $aTeemIpDiscovery['default_ip_status'] : 'allocated';
 			$this->bManageIpv6 = array_key_exists('manage_ipv6', $aTeemIpDiscovery) ? $aTeemIpDiscovery['manage_ipv6'] : 'no';
 			$this->bManageLogicalInterfaces = array_key_exists('manage_logical_interfaces', $aTeemIpDiscovery) ? $aTeemIpDiscovery['manage_logical_interfaces'] : 'no';
 
-			// Check if TeemIp IpDiscovery is installed or not
-			$oRestClient = new RestClient();
-			try {
-                $aResult = $oRestClient->Get('ModuleInstallation', "SELECT ModuleInstallation WHERE name = 'teemip-ip-discovery' AND installed >= '$sLastInstallDate'", 'version, installed', 1);
-				if (array_key_exists('objects', $aResult) && isset($aResult['objects'])) {
-					$this->bTeemIpIpDiscoveryIsInstalled = true;
-					Utils::Log(LOG_INFO, 'TeemIp IP Discovery is installed');
-				} else {
-					Utils::Log(LOG_INFO, 'TeemIp IP Discovery is NOT installed');
-				}
-			} catch (Exception $e) {
-				$sMessage = 'TeemIp IP Discovery is considered as NOT installed due to: ' . $e->getMessage();
-				if (is_a($e, "IOException")) {
-					Utils::Log(LOG_ERR, $sMessage);
-					throw $e;
-				}
-			}
-
-			// Check if TeemIp Network Management Extended is installed or not
-			$oRestClient = new RestClient();
-			try {
-                $aResult = $oRestClient->Get('ModuleInstallation', "SELECT ModuleInstallation WHERE name = 'teemip-network-mgmt-extended' AND installed >= '$sLastInstallDate'", 'version, installed', 1);
-				if (array_key_exists('objects', $aResult) && isset($aResult['objects'])) {
-					$this->bTeemIpNMEIsInstalled = true;
-					Utils::Log(LOG_INFO, 'TeemIp Network Management Extended is installed');
-				} else {
-					Utils::Log(LOG_INFO, 'TeemIp Network Management Extended is NOT installed');
-				}
-			} catch (Exception $e) {
-				$sMessage = 'TeemIp Network Management Extended is considered as NOT installed due to: ' . $e->getMessage();
-				if (is_a($e, "IOException")) {
-					Utils::Log(LOG_ERR, $sMessage);
-					throw $e;
-				}
-			}
-
-			// Check if TeemIp Zone Management is installed or not
-			$oRestClient = new RestClient();
-			try {
-                $aResult = $oRestClient->Get('ModuleInstallation', "SELECT ModuleInstallation WHERE name = 'teemip-zone-mgmt' AND installed >= '$sLastInstallDate'", 'version, installed', 1);
-				if (array_key_exists('objects', $aResult) && isset($aResult['objects'])) {
-					$this->bTeemIpZoneMgmtIsInstalled = true;
-					Utils::Log(LOG_INFO, 'TeemIp Zone Management extension is installed');
-				} else {
-					Utils::Log(LOG_INFO, 'TeemIp Zone Management extension is NOT installed');
-				}
-			} catch (Exception $e) {
-				$sMessage = 'TeemIp Zone Management extension is considered as NOT installed due to: ' . $e->getMessage();
-				if (is_a($e, "IOException")) {
-					Utils::Log(LOG_ERR, $sMessage);
-					throw $e;
-				}
-			}
+			// Check if TeemIP modules are installed
+			$this->bTeemIpIpDiscoveryIsInstalled = Utils::CheckModuleInstallation('teemip-ip-discovery', false, $oRestClient);
+			$this->bTeemIpNMEIsInstalled = Utils::CheckModuleInstallation('teemip-network-mgmt-extended', false, $oRestClient);
+			$this->bTeemIpZoneMgmtIsInstalled = Utils::CheckModuleInstallation('teemip-zone-mgmt', false, $oRestClient);
 		} else {
-			Utils::Log(LOG_INFO, 'As requested, TeemIp will not be considered.');
+			Utils::Log(LOG_INFO, 'As requested, TeemIP will not be considered.');
 		}
 	}
 
